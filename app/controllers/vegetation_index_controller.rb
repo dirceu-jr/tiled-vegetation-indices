648ffb31539e5b25ec3574c0b335d07670ceacfb
dirceupj <dirceuu@gmail.com>
2019-04-12 22:52:08 -0300

    fix bug in VI controller

--- app/controllers/vegetation_index_controller.rb
+++ app/controllers/vegetation_index_controller.rb
@@ -1,9 +1,11 @@
 class VegetationIndexController < ApplicationController
-  tile = params[:tile]
-  path = ""
-  tile_path = "#{path}/app/media/#{tile}"
-
-  data = VegetationIndex.apply_index(params[:index], tile_path, params[:min], params[:max], params[:band_order])
+  def index
+    tile = params[:tile]
+    path = ""
+    tile_path = "#{path}/app/media/#{tile}"
   
-  send_data(data.write_to_buffer(".png"), type: "image/png", disposition: "inline")
+    data = VegetationIndex.apply_index(params[:index], tile_path, params[:min], params[:max], params[:band_order])
+    
+    send_data(data.write_to_buffer(".png"), type: "image/png", disposition: "inline")
+  end
 end
