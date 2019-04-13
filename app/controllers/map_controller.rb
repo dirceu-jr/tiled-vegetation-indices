a1a2ef5295e78059f8e7dd33f41efffeadde416e
dirceupj <dirceuu@gmail.com>
2019-04-12 22:52:23 -0300

    add map draft

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,5 @@
+class MapController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/vegetation_index', to: 'vegetation_index#index', as: 'vegetation_index'
+  get '/map', to: 'map#index', as: 'map_index'
 end
