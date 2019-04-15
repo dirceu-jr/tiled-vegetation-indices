509d1a09cc14a14d53d2763da373647e84eb3662
dirceupj <dirceuu@gmail.com>
2019-04-14 21:50:12 -0300

    makes map#index root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/vegetation_index', to: 'vegetation_index#index', as: 'vegetation_index'
   get '/map', to: 'map#index', as: 'map_index'
+  
+  root to: "map#index"
 end
